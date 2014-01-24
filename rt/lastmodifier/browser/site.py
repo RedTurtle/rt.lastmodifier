92aaf00f1a866c5def8020b2f7403cb0857c3e8a
Andrea Cecchi <andrea.cecchi85@gmail.com>
2014-01-24 10:47:17 +0100

    hidden display_pub_date_in_byline field from controlpanel

---
+++ rt/lastmodifier/browser/site.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from plone.app.controlpanel.site import ISiteSchema
+from plone.app.controlpanel.site import SiteControlPanel as BaseSiteControlPanel
+from zope.formlib.form import FormFields
+
+
+class SiteControlPanel(BaseSiteControlPanel):
+    """
+    Hide display_pub_date_in_byline field.
+    Now we use the permissions
+    """
+
+    form_fields = FormFields(ISiteSchema).omit('display_pub_date_in_byline')
